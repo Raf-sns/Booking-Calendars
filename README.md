4e42257fdbe6da98908d4ecf562baacc7b8ab1cb
Raf-sns <contact@sns.pm>
2024-05-07 20:48:47 +0200

    V 2.0.0

---
+++ README.md
@@ -0,0 +1,110 @@
+## Booking Calendars
+A very very lightweight JavaScript library to display calendars of your online reservations.
+This library is very fast and was designed to avoid exponential redundancies.
+
+**See on this page :**
+[How to use it](#how-to-use-it-)
+[App Documentation](#app-documentation-)
+[Example of the Options object](#example-of-the-options-object-)
+[About Booking Calendars CSS](#about-booking-calendars-css-)
+[Example](#example-)
+[License](#license-)
+[Disclaimer](#disclaimer-)
+
+![Booking Calendars in action](/images/Screenshot_1.png)
+
+### How to use it ?
+1. Copy or download **Booking_Calendars.min.js** in `dist/Booking_Calendars.min.js`
+2. Add the CSS part of the library located in `dist/Booking_Calendars.min.css` in the `<head>...</head>` of your website.
+3. In the code of your HTML page create a tag `<div id="Booking_Calendars_Container"></div>`.
+4. Add the library using a `<script>` tag just before closing the `</body>` tag in your HTML code like this: `<script src="JS/Booking_Calendars.min.js">< /script>` if you placed the library in a `JS` folder or modify the file path according to your convenience.
+5. Create a JavaScript file that you will place below that of the library to adjust the options that you will pass to the function as well as the HTML element to target which will be the calendar container.
+6. Initialize the function with the 2 parameters: The HTML element and the options object and you will obtain your calendars with reserved periods and free periods.
+ ex. :
+Target the HTML element who will contain the calendars:
+ `var Element = document.getElementById('Booking_Calendars_Container');`
+ Set options to pass to the function:
+`var Options = { ... };`
+Run the library:
+`Booking_calendars(Element, Options);`
+
+### App Documentation :
+
+Function **Booking_calendars(Element, Options);**
+| Parameter | Description |
+| :--- | :--- |
+| `Element` | Target **one** HTML element which will be the container for the calendars. ex. `var Element = document.getElementById('Booking_Calendars_Container');`  |
+| `Options` | An object that sets the options of the library|
+
+Object **Options = { };**
+| Property | Value |
+| :--- | :--- |
+|`names_months`|An array containing the names of the months of the year, according to the desired language. ex.: `['January', 'February', 'March', 'April','May', 'June','July', 'August','September', 'October', 'November', 'December']` |
+`names_days` | An Array containing the first letters of the names of the days of the week, depending on the desired language. ex.: `['M', 'T', 'W', 'T', 'F', 'S', 'S']` |
+`months_range` | The desired monthly range, in array with 2 digits, if you want to display only a monthly range. ex. `[0,6]` to display only the first six months of the year. Pass here `false` as a parameter if you want to display all months of the year. |
+`year` | Year as a 4-character string ex. `'2024'` or `false` to display calendars for the current year. |
+`booked_dates` | An array of reserved date objects with 2 parameters: `date_start` and `date_end`, manage to obtain this data from your database, or pass it raw in the table ex.: `[ {date_start: '2024-03-01',date_end: '2024-03-04',},{start_date: '2024-03-04',date_end: '2024-03-08',}, ... ]`. **Note:** Dates formats must respect the `yyyy-mm-dd` format, like a classic date format in a database. |
+
+### Example of the Options object :
+```
+var Options = {
+    // set month names
+	names_months: [
+		'January', 'February', 'March', 'April',
+		'May', 'June', 'July', 'August',
+		'September', 'October', 'November', 'December'
+	],
+	// set day names
+	names_days: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+	// false or array of numbers: ex. [0,6] -> only first 6 months
+	months_range: false, // [3,9] || false
+	// year to process: Year in string 'yyyy' OR false for current year
+	year: '2024', // 'yyyy' || false
+	// objects array of booked dates
+	booked_dates : [
+    {
+      date_start : '2024-03-01',
+      date_end : '2024-03-04',
+    },
+    {
+      date_start : '2024-03-04',
+      date_end : '2024-03-08',
+    },
+    {
+      date_start : '2024-04-01',
+      date_end : '2024-04-04',
+    },
+    {
+      date_start : '2024-04-06',
+      date_end : '2024-04-09',
+    }
+    ]
+};
+// end Options for Booking_Calendars
+```
+### About Booking Calendars CSS :
+
+You will find two CSS files for the libray, one minified and the other non-minified in `dist/Booking_Calendars.css` and `dist/Booking_Calendars.min.css`.
+With the exception of the `#Booking_Calendars_Container` directive which styles the calendars container, other CSS declarations are all prefixed with the `BC-` prefix to avoid collisions.
+Feel free to modify the application's CSS file as you see fit in terms of colors, cell size, and characters.
+
+### Example :
+
+Download the `example` folder and open the index.html file with your favorite browser, you will see an example where the days are reserved every other day.
+In the `JS/main.js` script you will have an overview of the `Options` Array object and in the `JS/Booking_calendars.js` file you will be able to see the non-minified library code.
+
+### License :
+
+Under GNU AGPL license - Free - Open Source - Share your changes with the community - Credit the author.
+
+### Disclaimer :
+
+**This code comes without any warranty.**
+
+**Please report any bugs you encounter.**
+
+**Any contribution is welcome !**
+
+###
+
+❤ Developed by @Raf-sns - [Raphaël Castello / SNS - Web et informatique](https://sns.pm)
---
+++ example/JS/Booking_Calendars.js
@@ -0,0 +1,294 @@
+/**
+ * function Booking_calendars( Element, Options );
+ * @Copyright Raphaël Castello - 24/12/2023
+ * @License: AGPL-3.0
+ * @Organisation SNS - Web et informatique
+ * @Web www.sns.pm
+ * Vanilla javaScript for display reserved dates in calendars
+ * Version: 2.0.0 (07/05/2024)
+ */
+
+
+/**
+ * Booking_calendars( Element, Options );
+ * @param {string} Element HTML element to display calendars
+ * @param {object} Options Object of options
+ */
+const Booking_calendars = (Element, Options) => {
+
+
+	// Year is defined ?
+	Options.year =
+		(!Options.year) ? (new Date()).getFullYear() : Options.year;
+
+	/**
+	 * function Date_from_DB( date );
+	 * transform a date in DB format into a Date object
+	 * @param {string} date
+	 */
+	const Date_from_DB = (date) => {
+
+		let Splitted = date.split('-');
+
+		let Db_date =
+			new Date(parseInt(Splitted[0]),
+				parseInt(Splitted[1]) - 1,
+				parseInt(Splitted[2]));
+
+		Db_date.setHours(0, 0, 0, 0);
+
+		return Db_date;
+	};
+	/**
+	 * end function Date_from_DB( date );
+	 */
+
+	// index of array Optionsions.booked_dates
+	let Datas_index = 0;
+
+	/**
+	 * function Watch_Booked( DateObj );
+	 * function which checks if a day is booked
+	 * @param {object} DateObj JS Date Object
+	 */
+	const Watch_Booked = (DateObj) => {
+
+		// array of CSS classes to return
+		let ClassList = [];
+
+		// loop through booked datas
+		for (; Datas_index < Options.booked_dates.length; Datas_index++) {
+
+
+			// return Date object from booked_dates
+			let Date_DB_start =
+				Date_from_DB(Options.booked_dates[Datas_index].date_start);
+			let Date_DB_end =
+				Date_from_DB(Options.booked_dates[Datas_index].date_end);
+
+
+			if (DateObj.getTime() == Date_DB_start.getTime()) {
+
+				ClassList.push('BC-booked', 'BC-start');
+			}
+
+			if (DateObj.getTime() == Date_DB_end.getTime()) {
+
+				ClassList.push('BC-booked', 'BC-end');
+			}
+
+			if (DateObj.getTime() > Date_DB_start.getTime() &&
+				DateObj.getTime() < Date_DB_end.getTime()) {
+
+				ClassList.push('BC-booked');
+			}
+
+			// do not iterate over other dates - saving exponential processes
+			if (Date_DB_end.getTime() > DateObj.getTime()) {
+
+				break;
+			}
+
+		}
+		// end loop through datas
+
+		// don't push classes  "start" and "end" at the same time
+		if (ClassList.indexOf('BC-start') != -1 &&
+			ClassList.indexOf('BC-end') != -1) {
+
+			// empty array
+			ClassList.length = 0;
+			// push only .booked
+			ClassList.push('BC-booked');
+		}
+
+		return ClassList;
+	};
+	/**
+	 * end function Watch_Booked( DateObj );
+	 */
+
+
+	// define a range of months with a range if necessary
+	let Array_months = (!Options.months_range) ?
+		Options.names_months :
+		Options.names_months.slice(Options.months_range[0], Options.months_range[1]);
+
+	let Current_month = Options.names_months.indexOf(Array_months[0]);
+	let Calendar,
+		Day_week,
+		Day_week_base,
+		Month,
+		Month_name_tr,
+		Month_name_th,
+		Day,
+		Day_process,
+		Days_names_tr,
+		Day_name_td,
+		Day_tr,
+		Day_td,
+		Previous_days,
+		Days_left,
+		Prev_date,
+		Next_date,
+		Booked;
+
+	// loop all months
+	for (var i = 0; i <= Array_months.length - 1; i++) {
+
+		// create one Calendar
+		Calendar = document.createElement('table');
+		Calendar.classList.add('BC-style_calendar');
+
+		// create name month row
+		Month_name_tr = document.createElement('tr');
+
+		// create th name month colspan="7"
+		Month_name_th = document.createElement('th');
+		Month_name_th.setAttribute('colspan', '7');
+		Month_name_th.classList.add('BC-month_name');
+		Month_name_th.innerText = Array_months[i] + ` ` + Options.year;
+
+		// append month th to row name month
+		Month_name_tr.appendChild(Month_name_th);
+		// append month name row to Calendar
+		Calendar.appendChild(Month_name_tr);
+
+		// create Days_names_tr row
+		Days_names_tr = document.createElement('tr');
+		// append Days_names_tr to Calendar
+		Calendar.appendChild(Days_names_tr);
+
+		// create the names of the days of the week
+		for (var d = 0; d < Options.names_days.length; d++) {
+
+			// create day name
+			Day_name_td = document.createElement('td');
+			Day_name_td.classList.add('BC-days_names');
+			Day_name_td.innerText = Options.names_days[d];
+
+			// append day name to Days_names_tr row
+			Days_names_tr.appendChild(Day_name_td);
+		}
+		// end create the names of the days of the week
+
+		// create frist day of month
+		Day = 1;
+		Day_process = new Date(Options.year, Current_month, Day);
+		Day_process.setHours(0, 0, 0, 0);
+		Month = Day_process.getMonth();
+		Day_week = Day_process.getDay();
+		Day_week_base = Day_process.getDay();
+
+		// days row
+		Day_tr = document.createElement('tr');
+
+		// calcul previous days
+		Previous_days = (Day_week_base == 0) ? 6 : Day_week_base - 1;
+
+		// add days of previous month
+		for (var pd = Previous_days; pd > 0; pd--) {
+
+			// set prev. date
+			Prev_date = new Date(Options.year, Current_month, (Day - pd));
+			Prev_date.setHours(0, 0, 0, 0);
+
+			Day_td = document.createElement('td');
+			Day_td.classList.add('BC-day', 'BC-day_left');
+			Day_td.innerText = Prev_date.getDate();
+			Day_tr.appendChild(Day_td);
+		}
+		// end add days of previous month
+
+		// test current month
+		while (Current_month == Month) {
+
+			// create new day td
+			Day_td = document.createElement('td');
+			Day_td.classList.add('BC-day');
+			Day_td.innerText = Day_process.getDate();
+
+			// watch if this day is booked
+			Booked = Watch_Booked(Day_process);
+
+			// if booked
+			if (Booked.length != 0) {
+
+				// add classes if booked '.BC-start', 'BC-end', 'BC-booked'
+				Booked.forEach((item, i) => {
+
+					Day_td.classList.add(item);
+				});
+			}
+			// end if booked
+
+			// append one day in current row
+			Day_tr.appendChild(Day_td);
+
+			// append new days row
+			Calendar.appendChild(Day_tr);
+
+			// increment day && set new Date
+			Day++;
+			Day_process.setDate(Day);
+			Day_process.setHours(0, 0, 0, 0);
+
+			// test new month for empty last days left
+			if (Month != Day_process.getMonth()) {
+
+				// empty last days new month
+				if (Day_week != 0) {
+
+					// calcul nb days left
+					Days_left = 7 - Day_week;
+
+					// append days left to last current row
+					for (var dl = 0; dl < Days_left; dl++) {
+
+						// set next date
+						Next_date = new Date(Options.year, Month, (Day + dl));
+						Next_date.setHours(0, 0, 0, 0);
+
+						// insert days of new month
+						Day_td = document.createElement('td');
+						Day_td.classList.add('BC-day', 'BC-day_left');
+						Day_td.innerText = Next_date.getDate();
+						Day_tr.appendChild(Day_td);
+					}
+					// end for append days left
+				}
+				// empty last days new month
+			}
+			// end test new month for empty last days left
+
+			// set month
+			Month = Day_process.getMonth();
+
+			// set day of the week
+			Day_week = Day_process.getDay();
+
+			// if day week = 1 -> Monday -> set new days row
+			if (Day_week == 1) {
+
+				// create new row days
+				Day_tr = document.createElement('tr');
+			}
+
+		}
+		// end while : Current_month == Month
+
+
+		// add new month
+		Current_month++;
+
+
+		// append one calendar to element container
+		Element.appendChild(Calendar);
+
+	}
+	// end loop all months
+
+}
+/**
+ * end Booking_calendars( Element, Options );
+ */
